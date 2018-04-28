acd3839d5757cd8043bbd87dc7821bf881f69bdf
Jorne De Smedt <jornedesmedt@hotmail.com>
2018-04-28 10:05:40 +0200

    Fixed the error that was intentionally made in the previous commit but forgot to remove.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -177,7 +177,7 @@ int main(void)
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
