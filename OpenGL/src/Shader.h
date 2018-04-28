4a9f530cc77eb02345de4547ef82e7664a6ce5fb
Jorne De Smedt <jornedesmedt@hotmail.com>
2018-04-28 20:51:08 +0200

    Episode 19: Math

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -26,6 +28,7 @@ public:
 	void SetUniform1i(const std::string& name, int v0);
 	void SetUniform1f(const std::string& name, float v0);
 	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
 	int GetUniformLocation(const std::string &name);
 	ShaderProgramSource ParseShader(const std::string & filepath);
